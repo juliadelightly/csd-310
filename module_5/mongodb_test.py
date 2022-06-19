5737dd65f41059c8571d1587798459f0bfccfef9
juliadelightly <juliakathryndelightly@gmail.com>
2022-06-19 13:37:58 -0700

    commit changes to all files for module 5

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,14 +1,21 @@
+""" 
+Assignment: mongodb_test.py
+Julia Delightly
+Professor Sampson
+19 June 2022
+Description: Test program for connecting to a MongoDB Atlas cluster
+"""
+# import statement
 from pymongo import MongoClient
- 
-try:
-    conn = MongoClient()
-    print("Connected successfully!!!")
-except: 
-    print("Could not connect to MongoDB")
 
-# mongodb atlas url to connect python to mongodb using pymongo
-url = "mongodb+srv://admin:admin@cluster0.kdyeb.mongodb.net/?retryWrites=true&w=majority"
+# MongoDB connection string 
+url ="mongodb+srv://admin:admin@cluster0.kdyeb.mongodb.net/?retryWrites=true&w=majority"
 
-db = conn.pytech
+# connect to MongoDB cluster 
+client = MongoClient(url)
 
-students = db.students
+# connect pytech database
+db = client.pytech
+
+print("-- Pytech Collection List --")
+print(db.list_collection_names())
---
+++ module_5/pytech_insert.py
@@ -0,0 +1,124 @@
+""" 
+Assignment: pytech_insert.py
+Julia Delightly
+Professor Sampson
+19 June 2022
+Description: Test program for inserting new documents into student collection
+"""
+
+""" import statements """
+from pymongo import MongoClient
+
+# MongoDB connection string 
+url = "mongodb+srv://admin:admin@cluster0.kdyeb.mongodb.net/?retryWrites=true&w=majority"
+
+# connect to the MongoDB cluster 
+client = MongoClient(url)
+
+# connect pytech database
+db = client.pytech
+
+""" three student documents"""
+
+mason = {
+    "student_id": "1007",
+    "first_name": "Mason",
+    "last_name": "Bergenholtz",
+    "enrollments": [
+        {
+            "term": "Session 2",
+            "gpa": "4.0",
+            "start_date": "July 10, 2020",
+            "end_date": "September 14, 2020",
+            "courses": [
+                {
+                    "course_id": "CSD310",
+                    "description": "Database Development and Use",
+                    "instructor": "Professor Krasso",
+                    "grade": "A+"
+                },
+                {
+                    "course_id": "CSD320",
+                    "description": "Programming with Java",
+                    "instructor": "Professor Krasso",
+                    "grade": "A+"
+                }
+            ]
+        }
+    ]
+
+}
+
+ 
+alyssum = {
+    "student_id": "1008",
+    "first_name": "Alyssum",
+    "last_name": "Guerrero",
+    "enrollments": [
+        {
+            "term": "Session 2",
+            "gpa": "3.52",
+            "start_date": "July 10, 2020",
+            "end_date": "September 14, 2020",
+            "courses": [
+                {
+                    "course_id": "CSD310",
+                    "description": "Database Development and Use",
+                    "instructor": "Professor Krasso",
+                    "grade": "B+"
+                },
+                {
+                    "course_id": "CSD320",
+                    "description": "Programming with Java",
+                    "instructor": "Professor Krasso",
+                    "grade": "A-"
+                }
+            ]
+        }
+    ]
+}
+
+
+hannah = {
+    "student_id": "1009",
+    "first_name": "Hannah",
+    "last_name": "Wiser",
+    "enrollments": [
+        {
+            "term": "Session 2",
+            "gpa": "1.5",
+            "start_date": "July 10, 2020",
+            "end_date": "September 14, 2020",
+            "courses": [
+                {
+                    "course_id": "CSD310",
+                    "description": "Database Development and Use",
+                    "instructor": "Professor Krasso",
+                    "grade": "C"
+                },
+                {
+                    "course_id": "CSD 320",
+                    "description": "Programming with Java",
+                    "instructor": "Professor Krasso",
+                    "grade": "B"
+                }
+            ]
+        }
+    ]
+}
+
+# get the students collection 
+students = db.students
+
+# insert statements with output 
+print("\n  -- INSERT STATEMENTS --")
+thorin_student_id = students.insert_one(thorin).inserted_id
+print("  Inserted student record Thorin Oakenshield into the students collection with document_id " + str(thorin_student_id))
+
+bilbo_student_id = students.insert_one(bilbo).inserted_id
+print("  Inserted student record Bilbo Baggins into the students collection with document_id " + str(bilbo_student_id))
+
+frodo_student_id = students.insert_one(frodo).inserted_id
+print("  Inserted student record Frodo Baggins into the students collection with document_id " + str(frodo_student_id))
+
+input("\n\n  End of program, press any key to exit... ")
