ad086086b3d18248b0d673337cd0125f8195f9fe
juliadelightly <juliakathryndelightly@gmail.com>
2022-06-19 15:32:53 -0700

    cleaning up program formatting

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -17,22 +17,23 @@ client = MongoClient(url)
 
 # connect pytech database
 db = client.pytech 
+
 students = db.students 
 student_list = students.find({})
 
-# display message 
+# print header
 print("\n  -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
 
-# loop over the collection and output the results 
+# loop through collection and print results 
 for doc in student_list:
     print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
 
 # find document by student_id
-bilbo = students.find_one({"student_id": "1008"})
+alyssum = students.find_one({"student_id": "1008"})
 
-# output the results 
+# print results 
 print("\n  -- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --")
-print("  Student ID: " + bilbo["student_id"] + "\n  First Name: " + bilbo["first_name"] + "\n  Last Name: " + bilbo["last_name"] + "\n")
+print("  Student ID: " + alyssum["student_id"] + "\n  First Name: " + alyssum["first_name"] + "\n  Last Name: " + alyssum["last_name"] + "\n")
 
 # exit message 
 input("\n\n  End of program, press any key to continue...")
