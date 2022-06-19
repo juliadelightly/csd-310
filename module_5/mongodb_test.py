a223362d5dd898de459c842507aadb28bdbaed2c
juliadelightly <juliakathryndelightly@gmail.com>
2022-06-19 12:38:01 -0700

    commit on 'main'

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,1 +1,14 @@
+from pymongo import MongoClient
+ 
+try:
+    conn = MongoClient()
+    print("Connected successfully!!!")
+except: 
+    print("Could not connect to MongoDB")
 
+# mongodb atlas url to connect python to mongodb using pymongo
+url = "mongodb+srv://admin:admin@cluster0.kdyeb.mongodb.net/?retryWrites=true&w=majority"
+
+db = conn.pytech
+
+students = db.students
