375601829a6ea9d5a5a43475d51bb99fafa59e08
juliadelightly <juliakathryndelightly@gmail.com>
2022-06-25 12:23:13 -0700

    fixed syntax error

--- module_6/pytech_delete.py
+++ module_6/pytech_delete.py
@@ -18,37 +18,36 @@ client = MongoClient(url)
 # connect pytech database
 db = client.pytech
 students = db.students
-student_list = students.find({}) #without anything in {} returns all
+student_list = students.find({}) 
 
 print("\n  -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
 
-# loop through collection 
+# loop through current collection 
 for doc in student_list:
     print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
 
-# test document 
-test_doc = {
+# add temp document 
+temp_doc = {
     "student_id": "1010",
-    "first_name": "John",
-    "last_name": "Doe"
+    "first_name": "Nick",
+    "last_name": "Jonas"
 }
 
-# insert test document into MongoDB atlas 
-test_doc_id = students.insert_one(test_doc).inserted_id
+# insert temp_doc into MongoDB atlas 
+temp_doc_id = students.insert_one(temp_doc).inserted_id
 
-# insert statements with output 
 print("\n  -- INSERT STATEMENTS --")
-print("  Inserted student record into the students collection with document_id " + str(test_doc_id))
+print("  Inserted student record into the students collection with document_id " + str(temp_doc_id))
 
 # call find_one() method by student_id 1010
-student_test_doc = students.find_one({"student_id": "1010"})
+student_temp_doc = students.find_one({"student_id": "1010"})
 
 # display results 
 print("\n  -- DISPLAYING STUDENT TEST DOC -- ")
-print("  Student ID: " + student_test_doc["student_id"] + "\n  First Name: " + student_test_doc["first_name"] + "\n  Last Name: " + student_test_doc["last_name"] + "\n")
+print("  Student ID: " + student_temp_doc["student_id"] + "\n  First Name: " + student_temp_doc["first_name"] + "\n  Last Name: " + student_temp_doc["last_name"] + "\n")
 
-# call delete_one method to remove student_test_doc
-deleted_student_test_doc = students.delete_one({"student_id": "1010"})
+# call delete_one method to remove student_temp_doc
+deleted_student_temp_doc = students.delete_one({"student_id": "1010"})
 
 # find all students in collection 
 new_student_list = students.find({})
