3491b688861b473624bacf19fec07d07abdf1c1b
juliadelightly <juliakathryndelightly@gmail.com>
2022-07-02 21:13:40 -0700

    Adding queries document

---
+++ module_8/pysports_queries.py
@@ -0,0 +1,34 @@
+""" 
+Assignment: pysports_queries.py
+Julia Delightly
+Professor Sampson
+3 July 2022
+Description: Test program to query MySQL database tables through the terminal window and a Python program
+"""
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user" : "Julia",
+    "password" : "JKD@sql22!",
+    "host" : "127.0.0.1",
+    "database" : "pysports",
+    "raise_on_warnings" : True,
+}
+
+try:
+    db = mysql.connector.connect(**config)
+
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+
+    input("\n\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print(" The supplied username or password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(" The specified database does not exist")
+
+    else:
+        print(err)
