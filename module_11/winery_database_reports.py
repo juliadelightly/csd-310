2440bbac4a0c2caa3ee3ce27ab49e2d341a2b09f
juliadelightly <juliakathryndelightly@gmail.com>
2022-07-13 19:52:53 -0700

    added join statement and table code

--- module_11/winery_database_reports.py
+++ module_11/winery_database_reports.py
@@ -15,4 +15,38 @@ mydb = mysql.connector.connect(
   database="winery"
 )
 
-cursor = db.cursor()
+cursor = mydb.cursor()
+
+def printTable(results,cursor):
+    """Prints table in MySQL format"""
+    #source: https://stackoverflow.com/a/20383011
+    widths = []
+    columns = []
+    tavnit = '|'
+    separator = '+' 
+
+    for column,cd in enumerate(cursor.description):
+        width = len(cd[0])
+        for rowNumber in range(len(response)):
+            if len(str(response[rowNumber][column])) > width:
+                width = len(str(response[rowNumber][column]))
+        widths.append(width)
+        columns.append(cd[0])
+
+    for w in widths:
+        tavnit += " %-"+"%ss |" % (w,)
+        separator += '-'*w + '--+'
+
+    print(separator)
+    print(tavnit % tuple(columns))
+    print(separator)
+    for row in results:
+        print(tavnit % row)
+    print(separator)
+    print()
+
+cursor.execute("SELECT wine.name, distributor.units, distributor.distributor_name FROM wine INNER JOIN distributor ON wine.upc = distributor.upc ORDER BY units DESC;")
+
+response = cursor.fetchall()
+print("Wine sales by units:")
+printTable(response,cursor)
